ace8497ba25483b28df2aa15ffd42f8af92a1f64
a17sol <mishasolovyev@yandex.ru>
2024-01-08 23:41:13 +0200

    const. cities width, cleanup

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,6 @@ public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         MainWindowC mainWindow = new MainWindowC();
         mainWindow.setVisible(true);
-        DatabaseHandler dbhand= new DatabaseHandler();
-        Connection conn=dbhand.getDbConnection();
-        dbhand.read_data(conn,Const.AIRPORT_TABLE, new String[]{Const.AIRPORTS_COUNTRY});
-        String WhereSt=Const.AIRPORTS_COUNTRY+"="+'\''+"Russia"+'\'';
-        //System.out.println(WhereSt);
-        System.out.println(Arrays.toString(dbhand.read_distinct_column(conn, Const.AIRPORT_TABLE, Const.AIRPORTS_CITY,WhereSt)));
-        System.out.println(Arrays.toString(dbhand.describe_table(conn,Const.BUYER_Table)));
+
     }
-        }
+}
--- src/SearchPanel.java
+++ src/SearchPanel.java
@@ -15,6 +15,9 @@ public class SearchPanel extends JPanel {
     public SearchPanel() throws SQLException, ClassNotFoundException {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
+        DatabaseHandler dbhand = new DatabaseHandler();
+        Connection conn = dbhand.getDbConnection();
+
 //INTERFACE
 
 //Log In button
@@ -33,37 +36,16 @@ public class SearchPanel extends JPanel {
         add(Box.createVerticalGlue());
 
 //From, To, Two ways
-        DatabaseHandler dbhand = new DatabaseHandler();
-        Connection conn = dbhand.getDbConnection();
         add(Box.createVerticalGlue());
         JPanel fromToP = new JPanel();
         fromToP.add(new JLabel("From:"));
-        JComboBox Countries_from=new JComboBox(dbhand.read_distinct_column(conn, Const.AIRPORT_TABLE, Const.AIRPORTS_COUNTRY,""));
+
+        JComboBox Countries_from = new JComboBox(dbhand.read_distinct_column(conn, Const.AIRPORT_TABLE, Const.AIRPORTS_COUNTRY,""));
         fromToP.add(Countries_from);
+
         String[] cities= dbhand.read_distinct_column(conn, Const.AIRPORT_TABLE, Const.AIRPORTS_CITY,"");
-        JComboBox Cities_from=new JComboBox(cities);
-        String WhereSt=Const.AIRPORTS_COUNTRY+"="+'\''+"Israel"+'\'';
-        ActionListener al = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("A");
-                String WselectedCountry = Const.AIRPORTS_COUNTRY+"="+'\''+Countries_from.getSelectedItem()+'\'';
-                System.out.println(WselectedCountry);
-                Cities_from.removeAllItems();
-                if (WselectedCountry.equals(Const.AIRPORTS_COUNTRY+"=\'Any country\'")){
-                    WselectedCountry="";
-                }
-                String[] cities=dbhand.read_distinct_column(conn, Const.AIRPORT_TABLE, Const.AIRPORTS_CITY,WselectedCountry);
-                System.out.println(Arrays.toString(cities));
-                //Cities_from.addItem("aaaa");
-                for (String i:cities) {
-                    Cities_from.addItem(i);
-                }
-            }
-        };
-        Countries_from.addActionListener(al);
-        String WselectedCountry = Const.AIRPORTS_COUNTRY+"="+'\''+Countries_from.getSelectedItem()+'\'';
-        //
+        JComboBox Cities_from = new JComboBox(cities);
+        Cities_from.setPrototypeDisplayValue(longestString(cities));
         fromToP.add(Cities_from);
         fromToP.add(new JComboBox(dbhand.read_distinct_column(conn, Const.AIRPORT_TABLE, Const.AIRPORTS_ID,"")));
         fromToP.add(Box.createRigidArea(new Dimension(10, 0)));
@@ -119,5 +101,34 @@ public class SearchPanel extends JPanel {
                 );
             }
         });
+
+        Countries_from.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("A");
+                String WselectedCountry = Const.AIRPORTS_COUNTRY+"="+'\''+Countries_from.getSelectedItem()+'\'';
+                System.out.println(WselectedCountry);
+                Cities_from.removeAllItems();
+                if (WselectedCountry.equals(Const.AIRPORTS_COUNTRY+"=\'Any country\'")){
+                    WselectedCountry="";
+                }
+                String[] cities=dbhand.read_distinct_column(conn, Const.AIRPORT_TABLE, Const.AIRPORTS_CITY,WselectedCountry);
+                System.out.println(Arrays.toString(cities));
+                //Cities_from.addItem("aaaa");
+                for (String i:cities) {
+                    Cities_from.addItem(i);
+                }
+            }
+        });
+
+    }
+
+    private String longestString(String[] input) {
+        String tmp = "";
+        for (String i : input) {
+            if (i.length() > tmp.length()) {
+                tmp = i;
+            }
+        }
+        return tmp;
     }
 }
