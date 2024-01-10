d68e546c3ca7f314aa9c410af6abe44853cc5544
admin <olyazvon@gmail.com>
2024-01-10 16:26:49 +0200

    Search functions search(query), Arraylist[]search(query)

--- src/DatabaseHandler.java
+++ src/DatabaseHandler.java
@@ -1,81 +1,75 @@
+import com.privatejgoodies.common.base.Objects;
+
 import javax.xml.crypto.Data;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
 
 
 public class DatabaseHandler extends Configs {
     Connection dbConnection;
 
-    public Connection getDbConnection()
-            throws ClassNotFoundException, SQLException {
+    public Connection getDbConnection() {
+        //{
         String connectionString = "jdbc:oracle:thin" + ":" + dbUser + "/" +
                 dbPass + "@" + dbHost + ":" + dbPort + ":" + dbName;
-        Class.forName("oracle.jdbc.OracleDriver");
-        dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
-//       if (dbConnection != null) {
-//            System.out.println("Connected!");
-//        }
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
+        } catch (SQLException | ClassNotFoundException  e) {
+            throw new RuntimeException(e);
+        }
         return dbConnection;
     }
 
 
-    // ввыводит на экран, перечисленные поля таблицы,
-    // если массив пустой, выводит всю таблицу
-    public void read_data(String table_name, String[] column_names) throws SQLException, ClassNotFoundException {
-        System.out.println("Reading " + Arrays.toString(column_names) + "from " + table_name);
-        String columns = "";
-        if (column_names.length == 0) {
-            columns = "*";
-            column_names = describe_table(getDbConnection(), table_name);
-        } else {
-            columns = column_names[0];
-            for (int i = 1; i < column_names.length; i++) {
-                columns = columns + "," + column_names[i];
-            }
-        }
-        String query = String.format("SELECT " + columns + " FROM %s", table_name);
-
-        try (Statement statement = getDbConnection().createStatement();
-             ResultSet rs = statement.executeQuery(query)) {
-
-            while (rs.next()) {
-                for (int i = 0; i < column_names.length; i++) {
-                    System.out.print(rs.getString(column_names[i]) + " ");
-
-                }
-                System.out.println();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-////ввыводит в строковый массив уникальные значения столбца
-//    public String[] read_distinct_column(Connection conn, String table_name,String column_name) {
-//        System.out.println("Reading distinct " + column_name + " from " + table_name);
-//        String query = String.format("SELECT DISTINCT " + column_name + " FROM %s", table_name);
-//        String st="";
-//        int i = 0;
-//        try (Statement statement = conn.createStatement();
-//             ResultSet rs = statement.executeQuery(query)) {
-//
-//            while (rs.next()) {
-//                st+= rs.getString(column_name)+" ";
+//    // ввыводит на экран, перечисленные поля таблицы,
+//    // если массив пустой, выводит всю таблицу
+//    public void read_data(String table_name, String[] column_names) {
+//        System.out.println("Reading " + Arrays.toString(column_names) + "from " + table_name);
+//        String columns = "";
+//        if (column_names.length == 0) {
+//            columns = "*";
+//            column_names = describe_table(getDbConnection(), table_name);
+//        } else {
+//            columns = column_names[0];
+//            for (int i = 1; i < column_names.length; i++) {
+//                columns = columns + "," + column_names[i];
 //            }
+//        }
+//        String query = String.format("SELECT " + columns + " FROM %s", table_name);
+//
+//        Statement statement = null;
+//        try {
+//            statement = getDbConnection().createStatement();
 //        } catch (SQLException e) {
-//            e.printStackTrace();
+//            throw new RuntimeException(e);
 //        }
-//        String[]column= st.split(" ");
-//        String[]new_column=new String[column.length+1];
-//        for (int j=0;j<column.length;j++){
-//            new_column[j+1]=column[j];
+//        ResultSet rs = null;
+//        try {
+//            rs = statement.executeQuery(query);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
 //        }
-//        new_column[0]="Any "+column_name;
-//        return new_column;
+//
+//        while (true) {
+//            try {
+//                if (!rs.next()) break;
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//            for (int i = 0; i < column_names.length; i++) {
+//                try {
+//                    System.out.print(rs.getString(column_names[i]) + " ");
+//                } catch (SQLException e) {
+//                    throw new RuntimeException(e);
+//                }
+//
+//            }
+//            System.out.println();
+//        }
 //    }
+
 
     //ввыводит в строковый массив уникальные значения столбца c условием
     public String[] read_distinct_column(String table_name,
@@ -181,54 +175,58 @@ public class DatabaseHandler extends Configs {
 
     String cityByIATA(String iata) {
         return read_distinct_column(Const.AIRPORT_TABLE, Const.AIRPORTS_CITY,
-                (Const.AIRPORTS_ID+"=\'"+iata+"\'"))[1];
+                (Const.AIRPORTS_ID + "=\'" + iata + "\'"))[1];
     }
 
 
-    public void search_tickets(String iata_from, String iata_to, Data data_from) {
+
+        public String q_search_flights (String iata_from, String iata_to, Data data_from) {
         String query_all = String.format("SELECT fl.*,s_economy.seats_left_economy,s_business.seats_left_business " +
                 " FROM " + Const.FLIGHT_TABLE + " fl" + ",(" + seats_left("Business") + ") s_business,(" +
                 seats_left("Economy") + ") s_economy" +
                 " WHERE fl." + Const.FLIGHTS_ID + "=s_economy.flight_id and fl." + Const.FLIGHTS_ID + "=s_business.flight_id  " +
                 "and (seats_left_business>0 OR seats_left_economy>0)");
-        String from="";
-        if (iata_from.equals("Any iata")){from="";}
-        if(!iata_from.isEmpty()&&!(iata_from.equals("Any iata"))){
-            from=" AND "+ Const.FLIGHTS_FROM+"= '"+iata_from+"'";
+        String from = "";
+        if (iata_from.equals("Any iata")) {
+            from = "";
         }
-        String to="";
-        if (iata_to.equals("Any iata")){to="";}
-        if(!iata_to.isEmpty()&&!(iata_from.equals("Any iata"))){
-            to=" AND "+ Const.FLIGHTS_TO+"= '"+iata_to+"'";
+        if (!iata_from.isEmpty() && !(iata_from.equals("Any iata"))) {
+            from = " AND " + Const.FLIGHTS_FROM + "= '" + iata_from + "'";
         }
-        String stDataFrom="";
-        if(data_from != null){
-            stDataFrom=" AND "+ Const.FLIGHTS_DEPARTURE+"= '"+stDataFrom+"'";
+        String to = "";
+        if (iata_to.equals("Any iata")) {
+            to = "";
         }
-
-        String query=query_all+from+to+stDataFrom;
+        if (!iata_to.isEmpty() && !(iata_from.equals("Any iata"))) {
+            to = " AND " + Const.FLIGHTS_TO + "= '" + iata_to + "'";
+        }
+        String stDataFrom = "";
+        if (data_from != null) {
+            stDataFrom = " AND " + Const.FLIGHTS_DEPARTURE + "= '" + stDataFrom + "'";
+        }
+        String query = query_all + from + to + stDataFrom;
         System.out.println(query);
-        try (Statement statement = getDbConnection().createStatement();
-             ResultSet rs = statement.executeQuery(query)) {
-            while (rs.next()) {
-                System.out.print(rs.getString(1)+ " ");
-                System.out.print(rs.getString(2) + " ");
-                System.out.print(rs.getString(3) + " ");
-                System.out.print(rs.getString(4) + " ");
-                System.out.print(rs.getString(5) + " ");
-                System.out.print(rs.getString(6) + " ");
-                System.out.print(rs.getString(7) + " ");
-                System.out.print(rs.getString(8) + " ");
-                System.out.println(rs.getString(9) + " ");
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        return query;
 
     }
+    public ArrayList[] search_flights_print(String query) {
+        try (Statement statement = getDbConnection().createStatement();
+             ResultSet rs = statement.executeQuery(query)){
+            System.out.println(query);
+            int columns = rs.getMetaData().getColumnCount();
+            System.out.println(columns);
+            ArrayList[] searchResult = new ArrayList[columns];
+            while (rs.next()) {
+                for (int i = 1; i <= columns; i++) {
+                    searchResult[i].add(rs.getString(i));
+                }
+            }
+            return searchResult;
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     //возращает запрос свободных сидений по классу
      public String seats_left(String TicketClass) {
