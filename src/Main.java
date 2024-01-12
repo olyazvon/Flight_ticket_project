7a56fdc9993a792c68d131994cb514bf1df016fb
a17sol <mishasolovyev@yandex.ru>
2024-01-12 17:28:19 +0200

    Working multi-iata search

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//        MainWindowC mainWindow = new MainWindowC();
-        //mainWindow.setVisible(true);
+        MainWindowC mainWindow = new MainWindowC();
+        mainWindow.setVisible(true);
         DatabaseHandler dbhand= new DatabaseHandler();
         System.out.println(dbhand.read_seats_for_flight("RU1235"));
         System.out.println(dbhand.prices_for_flight("RU1235"));
--- src/SelectPanel.java
+++ src/SelectPanel.java
@@ -39,7 +39,7 @@ public class SelectPanel extends JPanel {
 
         dateSettings.setFormatForDatesCommonEra("dd.MM.yyyy");
         dateSettings.setFormatForDatesBeforeCommonEra("dd.MM.uuuu");
-        FlightTableModel tabMod = new FlightTableModel(dbhand.search_flights(dbhand.q_search_flights(null,null,null)));
+        FlightTableModel tabMod = new FlightTableModel(dbhand.search_flights(dbhand.q_search_flights(new String[]{"Any iata"},new String[]{"Any iata"},null)));
         table = new JTable(tabMod);
         //table.setDefaultEditor(Object.class, null);
         //table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -171,7 +171,7 @@ public class SelectPanel extends JPanel {
                     arrayTo = new String[] {(String) iataTo.getSelectedItem()};
                 }
                 System.out.println(arrayFrom[1]);
-                tabMod.data = dbhand.search_flights(dbhand.q_search_flights(arrayFrom[0], arrayTo[0], dateP.getDate()));
+                tabMod.data = dbhand.search_flights(dbhand.q_search_flights(arrayFrom, arrayTo, dateP.getDate()));
                 tabMod.fireTableDataChanged();
             }
         });
