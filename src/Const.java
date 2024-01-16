53db7297a0bed1a5f99c81664409e2b84410dcee
admin <olyazvon@gmail.com>
2024-01-16 18:15:41 +0200

    consts for new tables

--- src/Const.java
+++ src/Const.java
@@ -11,11 +11,27 @@ public class Const {
     public static final String  FLIGHTS_ARRIVAL="Arrival";
     public static final String  FLIGHTS_PRICE_ECONOM="PriceEconom";
     public static final String  FLIGHTS_PRICE_BUSINESS="PriceBusiness";
-    public static final String  BUYER_Table="buyers";
-    public static final String  BUYERS_ID="buyer_id";
-    public static final String  BUYERS_NAME="name";
-    public static final String  BUYERS_SURNAME="surname";
-    public static final String  BUYERS_PASSPORT="passport";
+
+    public static final String  BOOKING_TABLE="Bookings";
+    public static final String  BOOKING_NUMBER="BookingNumber";
+    public static final String  BOOKING_Total="Total";
+    public static final String  BOOKING_DATE="BookingDate";
+
+
+
+    public static final String  USER_TABLE="USERS";
+    public static final String  USER_ID="User_id";
+    public static final String  USER_NAME="CardHolderName";
+    public static final String  USER_CARD_DATE="CardDate";
+    public static final String  USER_CARD_NUMBER="CardNumber";
+    public static final String  USER_BOOKING_NUMBER="BookingNumber";
+
+    public static final String  PASSENGER_TABLE="PASSENGERS";
+    public static final String  PASSENGER_ID="passport";
+    public static final String  PASSENGER_FIRST_NAME="FirstName";
+    public static final String PASSENGER_LAST_NAME="LastName";
+    public static final String  PASSENGER_SEAT="Seat";
+    public static final String  PASSENGER_FLIGHT="Flight";
     public static final String  SEAT_TABLE="seats";
     public static final String  SEATS_FLIGHT_ID="flight_id";
     public static final String  SEAT="seat";
