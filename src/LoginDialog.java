3f8ad0e92e26962a93d365bf8136517efd0b9b5c
a17sol <mishasolovyev@yandex.ru>
2024-01-23 03:32:15 +0200

    LoginDialog simplification

--- src/LoginDialog.java
+++ src/LoginDialog.java
@@ -6,13 +6,13 @@ import java.awt.event.*;
 
 public class LoginDialog extends JDialog {
 
-    private static final long serialVersionUID = 1L;
+    //private static final long serialVersionUID = 1L;
 
     public JTextField tfLogin, tfPassword;
     public JButton btnOk, btnCancel;
 
     public LoginDialog(JFrame parent) {
-        super(parent, "Вход в систему");
+        super(parent, "Log In");
         // При выходе из диалогового окна работа заканчивается
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent we) {
@@ -22,6 +22,7 @@ public class LoginDialog extends JDialog {
         });
         // добавляем расположение в центр окна
         getContentPane().add(createGUI());
+        setResizable(false);
         // задаем предпочтительный размер
         pack();
         // выводим окно на экран
@@ -35,104 +36,57 @@ public class LoginDialog extends JDialog {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         // Определение отступов от границ ранели. Для этого используем пустую рамку
         panel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
+
         // Создание панели для размещения метки и текстового поля логина
         JPanel name = new JPanel();
         name.setLayout(new BoxLayout(name, BoxLayout.X_AXIS));
-        JLabel nameLabel = new JLabel("Имя:");
+        JLabel nameLabel = new JLabel("Login:", SwingConstants.RIGHT);
         name.add(nameLabel);
         name.add(Box.createHorizontalStrut(12));
         tfLogin = new JTextField(15);
+        tfLogin.setFont(new Font(null, Font.PLAIN, 14));
+        tfLogin.setBorder(BorderFactory.createLoweredBevelBorder());
         name.add(tfLogin);
+
         // Создание панели для размещения метки и текстового поля пароля
         JPanel password = new JPanel();
         password.setLayout(new BoxLayout(password, BoxLayout.X_AXIS));
-        JLabel passwrdLabel = new JLabel("Пароль:");
+        JLabel passwrdLabel = new JLabel("Password:", SwingConstants.RIGHT);
         password.add(passwrdLabel);
         password.add(Box.createHorizontalStrut(12));
         tfPassword = new JPasswordField(15);
+        tfPassword.setFont(new Font(null, Font.PLAIN, 14));
+        tfPassword.setBorder(BorderFactory.createLoweredBevelBorder());
         password.add(tfPassword);
         // Создание панели для размещения кнопок управления
-        JPanel flow = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 0));
-        //JPanel grid = new JPanel(new GridLayout(1, 2, 5, 0));
-        btnOk = new JButton("OK");
-        btnCancel = new JButton("Отмена");
+        JPanel flow = new JPanel(new FlowLayout(FlowLayout.RIGHT, 0, 0));
+        btnOk = new JButton("Log in");
+        //btnCancel = new JButton("Cancel");
         flow.add(btnOk);
-        flow.add(btnCancel);
+        //JLabel spacer = new JLabel("");
+        //spacer.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 5));
+        //flow.add(spacer);
+        //flow.add(btnCancel);
 
-        //flow.add(grid);
-        // Выравнивание вложенных панелей по горизонтали
-        JComponent[] components = new JComponent[]{name, password, panel, flow};
-        for (int i = 0; i < components.length; i++) {
-            components[i].setAlignmentX(Component.LEFT_ALIGNMENT);
-        }
-        // Выравнивание вложенных панелей по вертикали
-
-        JComponent[] components1 = new JComponent[]{tfLogin, tfPassword, nameLabel, passwrdLabel};
-        for (int i = 0; i < components1.length; i++) {
-            components1[i].setAlignmentX(Component.CENTER_ALIGNMENT);
-        }
         // Определение размеров надписей к текстовым полям
 
-        JComponent[] components2 = new JComponent[]{nameLabel, passwrdLabel};
-
-        // Установка компонентам одинаковых размеров
-        for (int i = 0; i < components2.length; i++) {
-            components2[i].setPreferredSize(new Dimension(60,20));
-            components2[i].setMinimumSize(new Dimension(60,20));
-            components2[i].setMaximumSize(new Dimension(60,20));
-        }
-
-
-        // Определение стандартного вида для кнопок
-
-        JButton[] buttons = new JButton[]{btnOk, btnCancel};
-//        // Устранение "бесконечной" высоты текстовых полей
-//        for (int i = 0; i < buttons.length; i++) {
-//            // Объект Insets хранит расстояние от текста до границ кнопки
-//            Insets margin = buttons[i].getMargin();
-//            margin.left = 12;
-//            margin.right = 12;
-//            buttons[i].setMargin(margin);
-//        }
-
-
-        Dimension size = tfLogin.getPreferredSize();
-        //	чтобы текстовое поле по-прежнему могло увеличивать свой размер в длину
-        size.width = tfLogin.getMaximumSize().width;
-        //	Определение максимального размера текстового поля
-        tfLogin.setMaximumSize(size);
-
-        Dimension size1 = tfPassword.getPreferredSize();
-        //	чтобы текстовое поле по-прежнему могло увеличивать свой размер в длину
-        size1.width = tfPassword.getMaximumSize().width;
-        //	Определение максимального размера текстового поля
-        tfPassword.setMaximumSize(size1);
+        nameLabel.setPreferredSize(new Dimension(63, 20));
+        passwrdLabel.setPreferredSize(new Dimension(63, 20));
 
         // Сборка интерфейса
         panel.add(name);
         panel.add(Box.createVerticalStrut(12));
         panel.add(password);
-        panel.add(Box.createVerticalStrut(17));
+        panel.add(Box.createVerticalStrut(12));
         panel.add(flow);
         // готово
         return panel;
-
     }
+
     // тестовый метод для проверки диалогового окна
     public static void main(String[] args) {
         new LoginDialog(new JFrame());
     }
 
 
-    private static int maximumElementPosition(int[] array)
-    {
-        int maxPos = 0;
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > array [maxPos])
-                maxPos = i;
-        }
-        return maxPos;
-    }
-    }
-
-
+}
